b9a7c7813be4eb3fcb584f4a4e1a114365e30874
rafaelcahya <cahyaputraugira95@gmail.com>
2022-03-13 19:52:36 +0700

    sign in sign up redesign

--- frontend/src/container/pages/Sign Up/Signup.jsx
+++ frontend/src/container/pages/Sign Up/Signup.jsx
@@ -1,52 +1,49 @@
 import React from 'react'
 
 import help_icon from '../../asset/icon/help-circle.svg'
-import google_icon from '../../asset/icon/google.png'
-import github_icon from '../../asset/icon/github.png'
 
 export default function Signup() {
     return (
         <>
-            <div className='signup black flex flex-col items-center gap-10 pt-10 pb-20'>
-                <p className='poppins-600 text-25'>Sign Up to Project Coding</p>
-                <form className='poppins text-15 flex flex-col gap-5 sm:gap-10'>
-                    <div className='flex flex-col sm:flex-row gap-5 sm:gap-10'>
-                        <div className='flex flex-col items-start gap-3'>
-                            <label htmlFor="">Username</label>
-                            <input type="text" placeholder='Input your username' className='border-gray width-20 gray2 px-5 py-2 rounded-lg outline-none' />
-                        </div>
-                        <div className='flex flex-col items-start gap-3'>
-                            <label htmlFor="">Email</label>
-                            <input type="email" placeholder='Input your email' className='border-gray width-20 gray2 px-5 py-2 rounded-lg outline-none' />
-                        </div>
-                    </div>
-                    <div className='flex flex-col sm:flex-row gap-5 sm:gap-10'>
-                        <div className='flex flex-col items-start gap-3'>
-                            <label htmlFor="">Password</label>
-                            <div className='border-gray flex items-center justify-between gap-5 width-20 px-1 py-1 rounded-lg pr-3' >
-                                <input type="password" placeholder='Input your password' className='gray2 px-4 py-1 w-full outline-none' />
-                                <img src={help_icon} alt="" width={17} />
+            <div className='bg_gray py-10 lg:py-24 letter_spacing_1 h-screen'>
+                <div className='black bg_white mx-auto p-14 rounded-xl box_shadow w-fit'>
+                    <div className='flex flex-col gap-10'>
+                        <div className='flex flex-col gap-2'>
+                            <p className='poppins-600 text_25 black'>Sign up</p>
+                            <div className='flex items-center gap-1 poppins text_14'>
+                                <p className='secondary_text'>Already a member?</p>
+                                <p className='blue'>Sign in</p>
                             </div>
                         </div>
-                        <div className='flex flex-col items-start gap-3'>
-                            <label htmlFor="">Confirm Password</label>
-                            <div className='border-gray flex items-center justify-between gap-5 width-20 px-1 py-1 rounded-lg pr-3' >
-                                <input type="password" placeholder='Input your confirm password' className='gray2 px-4 py-1 w-full outline-none' />
-                                <img src={help_icon} alt="" width={17}/>
+                        <form className='poppins text_14 grid grid-cols-1 lg:grid-cols-2 gap-7'>
+                            <div className='flex flex-col gap-3'>
+                                <label htmlFor="">Username</label>
+                                <input type="text" placeholder='Input your username' className='border_gray w-[350px] gray2 px-5 py-2 rounded-md outline-none' />
                             </div>
+                            <div className='flex flex-col gap-3'>
+                                <label htmlFor="">Email</label>
+                                <input type="text" placeholder='Input your email' className='border_gray w-[350px] gray2 px-5 py-2 rounded-md outline-none' />
+                            </div>
+                            <div className='flex flex-col items-start gap-3'>
+                                <label htmlFor="">Password</label>
+                                <div className='border_gray flex items-center justify-between gap-5 w-[350px] px-1 py-1 rounded-md pr-3' >
+                                    <input type="password" placeholder='Input your password' className='gray2 px-4 py-1 w-full outline-none' />
+                                    <img src={help_icon} alt="" width={17} />
+                                </div>
+                            </div>
+                            <div className='flex flex-col items-start gap-3'>
+                                <label htmlFor="">Confirm Password</label>
+                                <div className='border_gray flex items-center justify-between gap-5 w-[350px] px-1 py-1 rounded-md pr-3' >
+                                    <input type="password" placeholder='Input your confirm password' className='gray2 px-4 py-1 w-full outline-none' />
+                                    <img src={help_icon} alt="" width={17} />
+                                </div>
+                            </div>
+                        </form>
+                        <div className='bg_blue rounded-md'>
+                            <p className='white text text-center py-2'>Sign up</p>
                         </div>
                     </div>
-                    <div className='flex flex-col items-center gap-5'>
-                        <p className='bg-purple text-white rounded-lg py-3 cursor-pointer w-full text-center'>Sign Up</p>
-                        <div className='flex flex-col gap-10'>
-                            <p>Or Sign Up with</p>
-                            <div className='flex items-center gap-10'>
-                                <img src={google_icon} alt="" width={36}/>
-                                <img src={github_icon} alt="" width={36}/>
-                            </div>
-                        </div>
-                    </div>
-                </form>
+                </div>
             </div>
         </>
     )
--- frontend/src/container/pages/Sign in/Signin.jsx
+++ frontend/src/container/pages/Sign in/Signin.jsx
@@ -1,43 +1,38 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
 
 import help_icon from '../../asset/icon/help-circle.svg'
-import google_icon from '../../asset/icon/google.png'
-import github_icon from '../../asset/icon/github.png'
 
 export default function Signin(){
     return (
         <>
-            <div className='signin black flex flex-col items-center gap-10 pt-10 pb-20'>
-                <p className='poppins-600 text-25'>Sign In to your account</p>
-                <form className='poppins text-15 flex flex-col gap-5 sm:gap-10'>
-                    <div className='flex flex-col gap-5 sm:gap-10'>
-                        <div className='flex flex-col items-start gap-3'>
-                            <label htmlFor="">Username or Email</label>
-                            <input type="text" placeholder='Input your username or email' className='border-gray width-20 gray2 px-5 py-2 rounded-lg outline-none' />
+            <div className='bg_gray py-24 letter_spacing_1 h-screen'>
+                <div className='black bg_white mx-auto p-14 rounded-xl box_shadow w-fit'>
+                    <div className='flex flex-col gap-10'>
+                        <div className='flex flex-col gap-2'>
+                            <p className='poppins-600 text_25 black'>Sign in</p>
+                            <p className='poppins text_14 secondary_text'>Enter your credential to access your account</p>
                         </div>
-                        <div className='flex flex-col items-start gap-3'>
-                            <label htmlFor="">Password</label>
-                            <div className='border-gray flex items-center justify-between gap-5 width-20 px-1 py-1 rounded-lg pr-3' >
-                                <input type="password" placeholder='Input your password' className='gray2 px-4 py-1 w-full outline-none' />
-                                <img src={help_icon} alt="" width={17} />
+                        <form className='poppins text_14 flex flex-col gap-7'>
+                            <div className='flex flex-col gap-3'>
+                                <label htmlFor="">Username or Email</label>
+                                <input type="text" placeholder='Input your username or email' className='border_gray w-[350px] gray2 px-5 py-2 rounded-md outline-none' />
                             </div>
+                            <div className='flex flex-col items-start gap-3'>
+                                <div className='flex justify-between w-full'>
+                                    <label htmlFor="">Password</label>
+                                    <p className='poppins-600 blue'>Forgot password ?</p>
+                                </div>
+                                <div className='border_gray flex items-center justify-between gap-5 w-[350px] px-1 py-1 rounded-md pr-3' >
+                                    <input type="password" placeholder='Input your password' className='gray2 px-4 py-1 w-full outline-none' />
+                                    <img src={help_icon} alt="" width={17} />
+                                </div>
+                            </div>
+                        </form>
+                        <div className='bg_blue rounded-md'>
+                            <p className='white text text-center py-2'>Sign in</p>
                         </div>
                     </div>
-                    <div className='flex flex-col items-center gap-5'>
-                        <p className='bg-purple text-white rounded-lg py-3 cursor-pointer w-full text-center'>Sign In</p>
-                        <Link to='/reset-password'>
-                            <p className='purple hover:underline cursor-pointer text-center'>Reset Password</p>
-                        </Link>
-                        <div className='flex flex-col gap-10'>
-                            <p>Or Sign In with</p>
-                            <div className='flex items-center gap-10'>
-                                <img src={google_icon} alt="" width={36}/>
-                                <img src={github_icon} alt="" width={36}/>
-                            </div>
-                        </div>
-                    </div>
-                </form>
+                </div>
             </div>
         </>
     )
