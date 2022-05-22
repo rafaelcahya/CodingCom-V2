f9cf06cdd5750cffd5412e3da8e154f4ef0d5741
rafaelcahya <cahyaputraugira95@gmail.com>
2022-05-22 15:30:27 +0700

    ConfirmPassword miss

--- frontend/src/container/pages/Sign Up/Signup.jsx
+++ frontend/src/container/pages/Sign Up/Signup.jsx
@@ -144,7 +144,7 @@ export default function Signup() {
                                     'border-[#C9C9C9]'
                                 }`}>
                                     <input type={confPasswordShown ? "text" : "password"} placeholder='Input your confirm password' className='text-black text-[13px] px-4 py-2.5 w-full outline-none tracking-wide' onChange={(event)=>{
-                                        setConfPasswordShown(event.target.value)
+                                        setConfirmpassword(event.target.value)
                                     }}/>
                                     {
                                         confPasswordShown ? <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#666" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-eye stroke-[#666]" onClick={toggleConfPassword}><path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path><circle cx="12" cy="12" r="3"></circle></svg> : <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-eye-off stroke-[#666]" onClick={toggleConfPassword}><path d="M17.94 17.94A10.07 10.07 0 0 1 12 20c-7 0-11-8-11-8a18.45 18.45 0 0 1 5.06-5.94M9.9 4.24A9.12 9.12 0 0 1 12 4c7 0 11 8 11 8a18.5 18.5 0 0 1-2.16 3.19m-6.72-1.07a3 3 0 1 1-4.24-4.24"></path><line x1="1" y1="1" x2="23" y2="23"></line></svg>
